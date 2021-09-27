b5e605d8dcd31a7b68a99d46aceb2d89381f1c6e
Юрий Гасников <juragv@gmail.com>
2021-09-27 14:06:36 +0500

    Corrected names of metrics

--- ParserLibrary/Components.cs
+++ ParserLibrary/Components.cs
@@ -461,8 +461,8 @@ return true;
         public Sender sender=new LongLifeRepositorySender();
         public Step()
         {
-            sucMetric = Pipeline.metrics.getMetric("packReceived", false, true, "All packages, sended to utility");
-            errMetric = Pipeline.metrics.getMetric("packReceived", true, false, "All packages, sended to utility");
+            sucMetric = Pipeline.metrics.getMetric("packagesReceived", false, true, "All packages, sended to utility");
+            errMetric = Pipeline.metrics.getMetric("packagesReceived", true, false, "All packages, sended to utility");
         }
 
         Metrics.Metric sucMetric = null;
@@ -602,8 +602,8 @@ return true;
         {
             if (sendToRex == null)
             {
-                sendToRex = Pipeline.metrics.getMetric("SendToRex", false, true, "Transaction to DummySystem1");
-                sendToRexErr = Pipeline.metrics.getMetric("SendToRex", true, false, "Transaction to DummySystem1");
+                sendToRex = Pipeline.metrics.getMetric("sendToRex", false, true, "Sended transactions to DummySystem1");
+                sendToRexErr = Pipeline.metrics.getMetric("sendToRex", true, false, "Sended transactions to DummySystem1");
             }
         }
 
