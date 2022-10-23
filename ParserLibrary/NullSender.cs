6a5c2d04690d70e67e83cfc1b480cd94f74bc974
Vyacheslav Kotrachev <slavickkv@gmail.com>
2022-10-23 21:18:16 +0400

    Move HTTPSender and NullSender to their own files

---
+++ ParserLibrary/NullSender.cs
@@ -0,0 +1,9 @@
+namespace ParserLibrary;
+
+[Annotation("Пустой Sender")]
+public class NullSender:Sender
+{
+    public override TypeContent typeContent => TypeContent.internal_list;
+    //   public string IDResponsedReceiverStep = "";
+
+}
