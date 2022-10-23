e6fab247f07553e88e9e9b21a037597ed67d8cca
Vyacheslav Kotrachev <slavickkv@gmail.com>
2022-10-23 21:33:24 +0400

    Move alias producers to a separate file

--- ParserLibrary/Components.cs
+++ ParserLibrary/Components.cs
@@ -88,10 +88,6 @@ return true;
         }
     }
 
-    public abstract class AliasProducer
-    {
-        public abstract string getAlias(string originalValue);
-    }
     public class SensitiveAttribute:Attribute
     {
         public string NameSensitive;
@@ -100,42 +96,6 @@ return true;
             NameSensitive = name;   
         }
 
-    }
-    [Sensitive("PHONE")]
-    public class AliasPhone : AliasProducer
-    {
-   
-        public override string getAlias(string originalValue)
-        {
-            if (originalValue.Length > 3)
-                return $"{originalValue.Substring(0, 2)}*{originalValue.Substring(originalValue.Length-3)}";
-            return "*";
-        }
-    }
-
-    [Sensitive("PAN")]
-
-    public class AliasPan : AliasProducer
-    {
- 
-        public override string getAlias(string originalValue)
-        {
-            if (originalValue.Length >= 16)
-                return $"{originalValue.Substring(0, 4)}***{originalValue.Substring(originalValue.Length - 3)}";
-            return "*";
-        }
-    }
-
-    [Sensitive("FIO")]
-
-    public class AliasFIO : AliasProducer
-    {
-
-        public override string getAlias(string originalValue)
-        {
-            var tt =originalValue.Split(' ');
-            return tt.Select(query => query.Substring(0,1)).Aggregate((a, b) => a + " " + b+"."); ;
-        }
     }
 
 
