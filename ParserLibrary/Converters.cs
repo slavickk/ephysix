d1479ba979d79a1f9532c8618746dde0abebf0ea
Vyacheslav Kotrachev <slavickkv@gmail.com>
2022-10-23 21:30:44 +0400

    Move converters to a separate file

---
+++ ParserLibrary/Converters.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Data.HashFunction.xxHash;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ParserLibrary;
+
+public class Hash:HashOutput
+{
+    public int SizeInBits = 64;
+    public Hash()
+    {
+        hashConverter = new Hasher();
+    }
+}
+
+public class HashOutput : ConverterOutput
+{
+    public AliasProducer aliasProducer { get; set; }
+    public HashConverter hashConverter { get; set; }
+
+    public class OutItem
+    {
+        public string hash { get; set; }
+        public string alias { get; set; }
+    }
+
+    public AbstrParser.UniEl ConvertToNew(AbstrParser.UniEl el)
+    {
+        var value = el.Value.ToString();
+        AbstrParser.UniEl result = new AbstrParser.UniEl() { Name = el.Name };
+        if (aliasProducer == null)
+        {
+            result.Value = hashConverter.hash(value);
+            return result;
+        }
+        else
+        {
+            var el1 = new AbstrParser.UniEl(result) { Name = "h", Value = hashConverter.hash(value) };
+            var el2 = new AbstrParser.UniEl(result) { Name = "a", Value = aliasProducer.getAlias(value) };
+            return result;
+        }
+        //            return JsonSerializer.Serialize<OutItem>(new OutItem() { alias = aliasProducer.getAlias(value), hash = hashConverter.hash(value) });   
+    }
+
+    public override AbstrParser.UniEl Convert(string value, AbstrParser.UniEl input_el, AbstrParser.UniEl output_el)
+    {
+        if (aliasProducer == null)
+        {
+            output_el.Value = hashConverter.hash(value);
+            return output_el;
+        }
+        else
+        {
+            var el1 = new AbstrParser.UniEl(output_el) { Name = "h", Value = hashConverter.hash(value) };
+            var el2 = new AbstrParser.UniEl(output_el) { Name = "a", Value = aliasProducer.getAlias(value) };
+            return output_el;
+        }
+//            return JsonSerializer.Serialize<OutItem>(new OutItem() { alias = aliasProducer.getAlias(value), hash = hashConverter.hash(value) });   
+    }
+}
+
+public abstract class ConverterOutput
+{
+    public abstract AbstrParser.UniEl Convert(string value, AbstrParser.UniEl input_el, AbstrParser.UniEl output_el);
+}
+
+public class Hasher : HashConverter
+{
+    public int SizeInBits = 64;
+    bool init = false;
+
+    public Hasher()
+    {
+    }
+
+    IxxHash instance;
+
+    void Init()
+    {
+        if (!init)
+        {
+            init = true;
+            instance = xxHashFactory.Instance.Create(new xxHashConfig() { HashSizeInBits = SizeInBits });
+        }
+    }
+
+    public override string hash(string value)
+    {
+        Init();
+        return instance.ComputeHash(Encoding.ASCII.GetBytes(value)).AsHexString();
+    }
+}
+
+public class CryptoHash : HashConverter
+{
+    static public string pwd = "QWE123";
+
+    public override string hash(string value)
+    {
+        var data = Encoding.UTF8.GetBytes(pwd + value);
+//            string sHash;
+        using (SHA256 shaM = new SHA256Managed())
+        {
+            byte[] hash = shaM.ComputeHash(data);
+            return Convert.ToBase64String(hash);
+//                Console.WriteLine(sHash);
+        }
+        //          throw new NotImplementedException();
+    }
+}
+
+public abstract class HashConverter
+{
+    public abstract string hash(string value);
+}
