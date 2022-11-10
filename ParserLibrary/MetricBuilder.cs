a308fef609202c956c61d8bd463bd2c2a47c083d
YGASNIKOV-PARAL\ygasnikov <juragv@gmail.com>
2022-11-10 12:05:06 +0500

    fix

---
+++ ParserLibrary/MetricBuilder.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization;
+
+namespace ParserLibrary
+{
+
+
+    public class MetricBuilder
+    {
+        [YamlIgnore]
+        Metrics.MetricCounters metricCounter;
+
+        public void Init()
+        {
+            metricCounter = new Metrics.MetricCounters(Name, Description, labels.Select(ii => ii.Name).ToArray());
+        }
+        public async Task Fill(AbstrParser.UniEl rootElement)
+        {
+            int maxCount = -1;
+            int maxIndex = -1;
+            {
+                int i = 0;
+                foreach (var el in labels)
+                {
+                    el.current_values = el.Value.getAllObject(rootElement).ToArray();
+                    if (el.current_values.Length == 0)
+                        return;
+                    if (el.current_values.Length > maxCount)
+                    {
+                        maxCount = el.current_values.Length;
+                        maxIndex = i;
+                    }
+                    i++;
+                }
+            }
+            string[] values = new string[labels.Count];    
+            for (int i = 0; i < maxCount; i++)
+            {
+                for (int j=0; j < labels.Count;j++)
+                {
+                    var el = labels[j];
+                    int index = i;
+                    if( el.current_values.Length <= i )
+                        index=el.current_values.Length-1;
+                    values[j] = el.current_values[index].ToString();
+                }
+                metricCounter.AddCount(String.Join('/', values));
+            }
+            
+        }
+        public string Name;
+        public string Description;
+        public class Label
+        {
+            public string Name;
+            public OutputValue Value;
+            public object[] current_values;
+        }
+        public List<Label> labels= new List<Label>();
+    }
+}
--- ParserLibrary/StreamSender.cs
+++ ParserLibrary/StreamSender.cs
@@ -1,10 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Reflection;
+using System.Text.Json;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
 using Npgsql;
+using YamlDotNet.Core.Tokens;
 
 namespace ParserLibrary;
 
@@ -18,7 +24,7 @@ public class StreamSender:HTTPSender
     public static Metrics.MetricCount metricPerformanceStreams = new Metrics.MetricCount("StreamTime", "Stream peformance time");
     string getVal1(AbstrParser.UniEl el,Stream stream)
     {
-        var conv = stream.fields[el.Name].SensitiveConverter;
+        var conv = stream.fieldsDict[el.Name].SensitiveConverter;
         if (conv != null)
             return getVal(conv.ConvertToNew(el));
         else
@@ -70,8 +76,10 @@ public class StreamSender:HTTPSender
             public string Name { get; set; } = "";
             public string Type { get; set; }
             public string Detail { get; set; }
+
             public string SensitiveData { get; set; }
             HashOutput converter = null;
+            public bool? Calculated { get; set; }
             public HashOutput SensitiveConverter
             {
                 get
@@ -91,12 +99,24 @@ public class StreamSender:HTTPSender
         }
         public string Name { get; set; }
         public string Description { get; set; }
-        public Dictionary<string,Field> fields { get; set; } = new Dictionary<string,Field>();
+
+        public Dictionary<string,Field> fieldsDict { get; set; } = new Dictionary<string,Field>();
+        public Field[] fields
+        {
+            get
+            {
+                return fieldsDict.Values.ToArray(); 
+            }
+            set
+            {
+                fieldsDict = value.ToDictionary(p => p.Name);
+            }
+        }
     }
     Dictionary<string, Stream> streams = new Dictionary<string, Stream>();  
 
 
-    public string db_connection_string = "User ID=fp;Password=rav1234;Host=192.168.75.220;Port=5432;Database=fpdb;SearchPath=md;";
+    public string db_connection_string = "User ID=fp;Password=rav1234;Host=192.168.75.219;Port=5432;Database=fpdb;SearchPath=md;";
     public override string getTemplate(string key)
     {
         return formJson(getStream(streamName));
@@ -112,13 +132,14 @@ public class StreamSender:HTTPSender
         stream.Name = "";
         var conn = new NpgsqlConnection(db_connection_string);
         conn.Open();
-        using (var cmd = new NpgsqlCommand(@"select n.nodeid,n.name,a.val,np.name,'String',ap.val,asd.val,rl.toid from md_node n
+        using (var cmd = new NpgsqlCommand(@"select n.nodeid,n.name,a.val,np.name,'String',ap.val,asd.val,rl.toid,ascalc.val from md_node n
 inner join md_node_attr_val a  
 on ( a.nodeid=n.nodeid and attrid=22)
 inner join md_arc l on (l.toid=n.nodeid and l.isdeleted=false)
 inner join md_node np on (l.fromid=np.nodeid and np.isdeleted=false)
 inner join md_node_attr_val ap on ( ap.nodeid=np.nodeid and ap.attrid=22)
 left join md_node_attr_val asd on ( asd.nodeid=np.nodeid and asd.attrid=51)
+left join md_node_attr_val ascalc on ( ascalc.nodeid=np.nodeid and ascalc.attrid=100)
 left join md_arc rl on ( rl.fromid=np.nodeid and rl.typeid=16)
 where n.typeid=md_get_type('Stream') and n.name =@name and n.isdeleted=false
 ", conn))
@@ -133,7 +154,11 @@ where n.typeid=md_get_type('Stream') and n.name =@name and n.isdeleted=false
                         stream.Name = reader.GetString(1);
                         stream.Description = reader.GetString(2);
                     }
-                    stream.fields.Add(reader.GetString(3), new Stream.Field() { Name = reader.GetString(3), Type = reader.GetString(4), Detail =reader.IsDBNull(5)?"": reader.GetString(5),SensitiveData = reader.IsDBNull(6) ? null : reader.GetString(6), linkedColumn = reader.IsDBNull(7) ? null : reader.GetInt64(7) });
+                    if(reader.GetString(3)=="mbr")
+                    {
+
+                    }
+                    stream.fieldsDict.Add(reader.GetString(3), new Stream.Field() { Name = reader.GetString(3), Type = reader.GetString(4), Detail =reader.IsDBNull(5)?"": reader.GetString(5),SensitiveData = reader.IsDBNull(6) ? null : reader.GetString(6), linkedColumn = reader.IsDBNull(7) ? null : reader.GetInt64(7),  Calculated = reader.IsDBNull(8) ? false : true });
                 }
             }
         }
@@ -142,10 +167,10 @@ where n.typeid=md_get_type('Stream') and n.name =@name and n.isdeleted=false
         if(stream.Name =="")
         {
             stream.Name = key;
-            stream.fields.Add("stream",new Stream.Field() { Name = "stream", Detail = "Name of stream", Type = "String" });
-            stream.fields.Add("originalTime",new Stream.Field() { Name = "originalTime", Detail = "Time of stream", Type = "DateTime" });
+            stream.fieldsDict.Add("stream",new Stream.Field() { Name = "stream", Detail = "Name of stream", Type = "String" });
+            stream.fieldsDict.Add("originalTime",new Stream.Field() { Name = "originalTime", Detail = "Time of stream", Type = "DateTime" });
         }
-        stream.fields.Where(ii => !string.IsNullOrEmpty(ii.Value.SensitiveData)).Select(ii1 => ii1.Value.SensitiveConverter);
+        stream.fieldsDict.Where(ii => !string.IsNullOrEmpty(ii.Value.SensitiveData)).Select(ii1 => ii1.Value.SensitiveConverter);
 
         streams.TryAdd(key, stream);
         return (stream);
@@ -156,9 +181,72 @@ where n.typeid=md_get_type('Stream') and n.name =@name and n.isdeleted=false
         getStream(streamName);
         base.Init(owner);
     }
+
+ 
+    public class StreamConsul
+    {
+        public class Field
+        {
+            public string Name { get; set; }
+            public string Type { get; set; }
+            public string Detail { get; set; }
+            public bool Calculated { get; set; }
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Field> fields { get; set; }
+    }
+
+
+    private static readonly JsonSerializerOptions jsonSerializerOptions
+      = new JsonSerializerOptions
+      {
+          PropertyNameCaseInsensitive = true,
+          PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+      };
+    private static async Task SendPutAsync(HttpClient httpClient,string addr,StreamConsul post)
+    {
+        var jsonContent = JsonSerializer.Serialize(post, jsonSerializerOptions);
+        using var httpContent = new StringContent
+            (jsonContent, Encoding.UTF8, "text/json");
+        
+        using var response = await httpClient.PutAsync(addr, httpContent);
+
+        response.EnsureSuccessStatusCode();
+    }
+    static public void ToConsul(Stream stream)
+    {
+
+        HttpClient httpClient = new HttpClient();
+        StreamConsul consul = new StreamConsul() { Name = stream.Name, Description = stream.Description, fields = stream.fields.Select(ii => new StreamConsul.Field() { Name = ii.Name, Detail = ii.Detail, Type = ii.Type,Calculated=(bool)ii.Calculated }).ToList() };
+        string CONSUL_ADDR = "http://192.168.75.204:8500";
+        var res =httpClient.PutAsJsonAsync<StreamConsul>($"{CONSUL_ADDR}/v1/kv/ROOT/STREAMS/Schemas/{stream.Name}", consul,new JsonSerializerOptions()).ContinueWith(ii =>
+        {
+            if (ii.IsCompletedSuccessfully)
+            { }
+        });
+
+        /*
+        if (stream.fields.Count(ii => ii.Calculated == true) == 0)
+            httpClient.DeleteAsync($"{CONSUL_ADDR}/v1/kv/ROOT/STREAMS/Schemas/Calculated/{stream.Name}").ContinueWith(ii =>
+            {
+                if (ii.IsCompletedSuccessfully)
+                { }
+            });
+        else
+        {
+            httpClient.PutAsJsonAsync<string[]>($"{CONSUL_ADDR}/v1/kv/ROOT/STREAMS/Schemas/Calculated/{stream.Name}", stream.fields.Where(ii=>ii.Calculated==true).Select(i1=>i1.Name).ToArray(), new JsonSerializerOptions()).ContinueWith(ii =>
+            {
+                if (ii.IsCompletedSuccessfully)
+                { }
+            });
+        }
+        */
+    }
     private static string formJson(Stream stream)
     {
-        return "{" + string.Join<string>(",", stream.fields.Select(ii => $"\"{ii.Value.Name}\":\"{((ii.Value.Name=="stream")?stream.Name:"")}\"")) + "}";
+        return "{" + string.Join<string>(",", stream.fieldsDict.Select(ii => $"\"{ii.Value.Name}\":\"{((ii.Value.Name=="stream")?stream.Name:"")}\"")) + "}";
     }
 
     public override void setTemplate(string key, string body)
