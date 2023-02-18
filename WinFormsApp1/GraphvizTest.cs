169ba060c023503f5c5028fb4b35cba594778e07
Юрий Гасников <juragv@gmail.com>
2023-02-18 19:12:48 +0500

    Fix

---
+++ WinFormsApp1/GraphvizTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GraphVizWrapper;
+using GraphVizWrapper.Commands;
+using GraphVizWrapper.Queries;
+
+
+namespace WinFormsETLPackagedCreator
+{
+    public class GraphvizTest
+    {
+
+        public static byte[] test()
+        {
+            var getStartProcessQuery = new GetStartProcessQuery();
+            var getProcessStartInfoQuery = new GetProcessStartInfoQuery();
+            var registerLayoutPluginCommand = new RegisterLayoutPluginCommand(getProcessStartInfoQuery, getStartProcessQuery);
+
+            // GraphGeneration can be injected via the IGraphGeneration interface
+
+            var wrapper = new GraphGeneration(getStartProcessQuery,
+                                              getProcessStartInfoQuery,
+                                              registerLayoutPluginCommand);
+
+            byte[] output = wrapper.GenerateGraph("digraph{a -> b; b -> c; c -> a;}", Enums.GraphReturnType.Png);
+
+            Bitmap bmp;
+            using (MemoryStream mStream = new MemoryStream())
+            {
+              //  byte[] pData = blob;
+                mStream.Write(output, 0, Convert.ToInt32(output.Length));
+                bmp = new Bitmap(mStream, false);
+            }
+           /* using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.DrawImage(new Bitmap((@"C:\Users\Mena\Desktop\1.png"), new Point(182, 213));
+            }*/
+     //       pictureBox2.Image = bmp;
+            return output;
+        }
+
+    }
+}
