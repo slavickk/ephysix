5c5a03ae72a774ef7ca5d270d64c656345af7030
Vyacheslav Kotrachev <slavickkv@gmail.com>
2023-04-17 12:32:01 +0700

    Add a IReceiver-based TestReceiver

--- ParserLibrary/TestReceiver.cs
+++ ParserLibrary/TestReceiver.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using System.Threading.Tasks;
+using PluginBase;
 
 namespace ParserLibrary;
 
@@ -23,5 +24,39 @@ public class TestReceiver:Receiver
             }
         }
 
+    }
+}
+
+/// <summary>
+/// A drop-in replacement for the TestReceiver that uses the new IReceiver interface.
+/// TODO: confirm TestReceiverV2 can be used instead of TestReceiver, then delete TestReceiver
+/// and rename TestReceiverV2 to TestReceiver.
+/// </summary>
+public class TestReceiverV2 : IReceiver
+{
+    public string path;
+    public string pattern = "";
+
+    public IReceiverHost host { get; set; }
+
+    public bool cantTryParse { get; set; }
+
+    public bool debugMode { get; set; }
+
+    public async Task start()
+    {
+        foreach (var file_name in ((pattern == "") ? new string[] { path } : Directory.GetFiles(path, pattern)))
+        {
+            using (StreamReader sr = new StreamReader(file_name))
+            {
+                var body = sr.ReadToEnd();
+                await host.signal(body, null);
+            }
+        }
+    }
+
+    public Task sendResponse(string response, object context)
+    {
+        throw new System.NotImplementedException("The TestReceiver is not supposed to send responses");
     }
 }
