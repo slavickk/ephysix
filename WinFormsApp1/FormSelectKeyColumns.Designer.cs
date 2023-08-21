d3fde6f4be5e4676c6672921dac298809e828315
YGASNIKOV-PARAL\ygasnikov <juragv@gmail.com>
2023-08-21 16:47:04 +0500

    Fix5

---
+++ WinFormsApp1/FormSelectKeyColumns.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace WinFormsETLPackagedCreator
+{
+    partial class FormSelectKeyColumns
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(1, 1);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(214, 310);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.Location = new System.Drawing.Point(60, 317);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormSelectKeyColumns
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(220, 345);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Name = "FormSelectKeyColumns";
+            this.Text = "FormSelectKeyColumns";
+            this.Load += new System.EventHandler(this.FormSelectKeyColumns_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CheckedListBox checkedListBox1;
+        private Button button1;
+    }
+}
---
+++ WinFormsApp1/FormSelectKeyColumns.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFormsETLPackagedCreator
+{
+    public partial class FormSelectKeyColumns : Form
+    {
+        string[] columns;
+        public FormSelectKeyColumns(string[] columns)
+        {
+            this.columns = columns;
+            InitializeComponent();
+        }
+        public List<string> keyColumns= new List<string>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            foreach(var check in checkedListBox1.CheckedItems)
+            {
+                keyColumns.Add(check.ToString());
+            }
+        }
+
+        private void FormSelectKeyColumns_Load(object sender, EventArgs e)
+        {
+            foreach(string column in columns)
+            {
+                checkedListBox1.Items.Add(column);
+            }
+        }
+    }
+}
