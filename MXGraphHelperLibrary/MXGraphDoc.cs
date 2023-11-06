28c1ca9b6eb07264a15e88a3109f48148c5af670
Юрий Гасников <juragv@gmail.com>
2023-11-06 13:22:25 +0500

    fix6

--- MXGraphHelperLibrary/MXGraphDoc.cs
+++ MXGraphHelperLibrary/MXGraphDoc.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Runtime.CompilerServices;
+using System.Text.Json;
 
 namespace MXGraphHelperLibrary
 {
@@ -16,6 +17,14 @@ namespace MXGraphHelperLibrary
  
     public class MXGraphDoc
     {
+        public void Save(string path=@"C:\d\ex.json")
+        {
+            using(StreamWriter sw = new StreamWriter(path))
+            {
+                sw.Write(JsonSerializer.Serialize<MXGraphDoc>(this
+                    , new JsonSerializerOptions { IgnoreNullValues = true, WriteIndented = true }));
+            }
+        }
         public class Box
         {
             public class Body
@@ -26,10 +35,19 @@ namespace MXGraphHelperLibrary
                     {
                         public class Item
                         {
+
+                            public void AddBoxLink(Box box,string id,int typeLink=2)
+                            {
+                                if (box_links == null)
+                                    box_links = new List<BoxLink>();
+                                box_links.Add(new BoxLink() { link = new BoxLink.Link() { typelink=typeLink, box_id = box.id + ":" + id } });
+                            }
                             public class BoxLink
                             {
                                 public class Link
                                 {
+
+                                    public int typelink { get; set; } = 1;
                                     public string box_id { get; set; }
                                 }
                                 public Link link { get; set; }
@@ -41,14 +59,14 @@ namespace MXGraphHelperLibrary
                             public List<BoxLink> box_links { get; set; }
                         }
 
-                        public JsonElement json { get; set; }
+                        public JsonElement? json { get; set; } = null;
                         public Item item { get; set; }
                     }
                     public List<Column> columns { get; set; }
                 }
                 public List<Header> header { get; set; }
                 public List<Row> rows { get; set; }
-                public JsonElement json { get; set; }
+                public JsonElement? json { get; set; } = null;
             }
             public class Header
             {
@@ -77,6 +95,59 @@ namespace MXGraphHelperLibrary
         public List<Box> boxes { get; set; }
     }
 
+
+    public class JsonHelper
+    {
+        string Name;
+
+        Dictionary<string,object> root= new Dictionary<string, object>();
+        public JsonHelper(string name) 
+        {
+            root.Add(name , new Dictionary<string, object>());
+            Name = name;
+        }
+        public void AddVal(string val)
+        {
+            var first = root[Name] as Dictionary<string,object>;
+//            var firstName = Name;
+            foreach(var el in val.Split("/"))
+            {
+                object valw;
+                if (!first.TryGetValue(el, out valw))
+                {
+                    first.Add(el, new Dictionary<string, object>());
+                    first = first[el] as Dictionary<string, object>;
+                }
+                else
+                    first = valw as Dictionary<string, object>;
+            }
+        }
+
+        void An(string id,Dictionary<string, object> dict)
+        {
+            if(dict.Count==0)
+            {
+
+                dict.Add("box_id", id);
+            } else
+            {
+                foreach(var el in dict.Keys)
+                {
+                    An(el, dict[el] as Dictionary<string,object>);
+                }
+            }
+        }
+
+        public string getJsonBody()
+        {
+            An(Name,root[Name] as Dictionary<string, object>);
+
+
+
+            return JsonSerializer.Serialize<Dictionary<string,object>>(root);
+        }
+
+    }
 
     
 
