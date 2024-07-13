76b21976c6c2c8df1461c1be468935512bd2ee7b
Vyacheslav Kotrachev <slavickkv@gmail.com>
2024-07-13 14:15:14 +0700

    Move PostgreSQL test from ParserLibrary to ParserLibrary.Tests. Use Testcontainers for it.

--- ParserLibrary.Tests/TestPostgres.cs
+++ ParserLibrary.Tests/TestPostgres.cs
@@ -16,22 +16,60 @@
 
 using NUnit.Framework;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using ParserLibrary;
+using Npgsql;
+using Testcontainers.PostgreSql;
 
 namespace ParserLibrary.Tests
 {
     [TestFixture]
     public class TestPostgresTest
     {
-        [Test]
-        public async Task  Begin()
+        private PostgreSqlContainer _postgresContainer;
+        
+        [OneTimeSetUp]
+        public async Task OneTimeSetUp()
         {
-            await TestPostgres.test();
+            _postgresContainer = new PostgreSqlBuilder()
+                .WithDatabase("my_database")
+                .Build();
+            
+            await _postgresContainer.StartAsync();
         }
+        
+        [OneTimeTearDown]
+        public async Task OneTimeTearDown()
+        {
+            await _postgresContainer.DisposeAsync();
+        }
+            
+        [Test]
+        public async Task Begin()
+        {
+            var connString = _postgresContainer.GetConnectionString();
 
+            await using var conn = new NpgsqlConnection(connString);
+            await conn.OpenAsync();
+
+            const string command = "CREATE TABLE IF NOT EXISTS public.data1(id integer NOT NULL, some_field varchar(20))";
+            await using (var cmd = new NpgsqlCommand(command, conn))
+                await cmd.ExecuteNonQueryAsync();
+
+            // Insert some data
+            await using (var cmd = new NpgsqlCommand("INSERT INTO data1 (id, some_field) VALUES (@id, @p)", conn))
+            {
+                cmd.Parameters.AddWithValue("id", 123);
+                cmd.Parameters.AddWithValue("p", "Hello world");
+                await cmd.ExecuteNonQueryAsync();
+            }
+
+            // Retrieve all rows
+            await using (var cmd = new NpgsqlCommand("SELECT some_field FROM data1", conn))
+            await using (var reader = await cmd.ExecuteReaderAsync())
+            {
+                while (await reader.ReadAsync())
+                    Console.WriteLine(reader.GetString(0));
+            }
+        }
     }
 }
